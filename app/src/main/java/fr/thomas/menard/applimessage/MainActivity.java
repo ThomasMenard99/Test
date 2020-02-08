68f74e933c1326933d7da9a62ac1cae5eca3ab83
Thomas Menard <thomasmenard63@gmail.com>
2020-02-08 12:13:26 +0530

    test

---
+++ app/src/main/java/fr/thomas/menard/applimessage/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.thomas.menard.applimessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
